81202ee88bbd6540e3edef50ceaa7dbd08eadf9d
Michael Stapelberg <stapelberg@google.com>
2023-03-05 10:37:31 +0100

    set CGO_ENABLED=0 to ensure rtr7-build-kernel runs in Docker

--- cmd/rtr7-rebuild-kernel/kernel.go
+++ cmd/rtr7-rebuild-kernel/kernel.go
@@ -118,7 +118,7 @@ func main() {
 	defer os.RemoveAll(tmp)
 
 	cmd := exec.Command("go", "install", "github.com/rtr7/kernel/cmd/rtr7-build-kernel")
-	cmd.Env = append(os.Environ(), "GOOS=linux", "GOBIN="+tmp)
+	cmd.Env = append(os.Environ(), "GOOS=linux", "GOBIN="+tmp, "CGO_ENABLED=0")
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("%v: %v", cmd.Args, err)
